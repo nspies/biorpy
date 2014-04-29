ab37803c0b84e59532e860a34de2e071044661ea
Noah Spies <nspies@stanford.edu>
2014-04-28 18:57:22 -0700

    adding svg support to interactive

--- biorpy/interactive.py
+++ biorpy/interactive.py
@@ -9,23 +9,38 @@ class InlineImage(object):
     def __init__(self):
         self.running = False
         self.directory = None
-    def start(self):
+
+    def start(self, format="svg", **kwdargs):
         if self.running:
             return
         self.running = True
         self.directory = tempfile.mkdtemp()
-        r.png("{}/Rplots%03d.png".format(self.directory))
+        self.format = format
+
+        if self.format == "svg":
+            r.svg("{}/Rplots%03d.svg".format(self.directory), **kwdargs)
+        elif self.format == "png":
+            r.png("{}/Rplots%03d.png".format(self.directory), **kwdargs)
+        else:
+            raise Exception("Unknown image format:{}".format(format))
+
     def finish(self):
         if not self.running:
             return
         self.running = False
         r.devoff()
         
-        imageFiles = glob("{}/Rplots*png".format(self.directory))
+        if self.format == "svg":
+            imageFiles = glob("{}/Rplots*svg".format(self.directory))
+            image_type = 'image/svg+xml'
+        elif self.format == "png":
+            imageFiles = glob("{}/Rplots*png".format(self.directory))
+            image_type = "image/png"
         images = [open(imgfile, 'rb').read() for imgfile in imageFiles]
 
         for image in images:
-            publish_display_data("biorpy", {'image/png': image})
+            publish_display_data("biorpy", {image_type: image})
+            # publish_display_data("biorpy", {'image/png': image})
 
         rmtree(self.directory)
 
